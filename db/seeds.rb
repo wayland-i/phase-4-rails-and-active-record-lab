9ed5efc46697adb12e3e3c5ea08de6f571587258
Ian Iversen <ianiversen1@gmail.com>
2022-08-30 10:34:33 -0400

    commit tree 4e989c9c4f77d6a8f9611d399df73f8f6d219aab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Student.create(first_name: "Dewayne", last_name: "Johnson", grade: 100)
